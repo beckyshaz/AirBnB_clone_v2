520d76cc9468fe7a409ff53f38e2abe68b0bf473
beckyshaz <sharonsakah8@gmail.com>
2024-07-27 06:33:20 -0700

    flask with route variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" flask app with routes returning hello hbn,
+hbnb and c plus the value of text"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    space_text = text.replace('_', ' ')
+    return f'C {space_text}'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
