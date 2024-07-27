99e44b6d7d431c25ace11bdcf91c2ff7a9be3192
beckyshaz <sharonsakah8@gmail.com>
2024-07-27 08:02:25 -0700

    typecasting an argument in flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,5 +29,10 @@ def python(text='is cool'):
     return f'Python {space_text}'
 
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
